a26bfb67d70fd779dc95563684bbea8f10a2c5ce
Neerrr001 <neeragarwal711@gmail.com>
2026-07-31 12:13:21 +0530

    added func (if 0 on display then add number) else (append to it)

--- html-calc-app/script.js
+++ html-calc-app/script.js
@@ -1,33 +1,22 @@
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
+const display = calculator.querySelector(".calculator__display")
 
 keys.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
-    
-    const key = e.target;
-    const action = key.dataset.action;
+  if(e.target.matches('button')){
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    console.log(key, action, keyContent, displayedNum)
 
-    if (!action) {
-      console.log("number-key");
-    }
-    if (
-      action === "add" ||
-      action === "subtract" ||
-      action === "multiply" ||
-      action === "divide"
-    ) {
-      console.log("operatory key");
-    }
-    if (action === "decimal") {
-      console.log("decimal key!");
+    if(!action){
+      if(displayedNum === '0'){
+        display.textContent = keyContent;
+      }else{
+        display.textContent = displayedNum + keyContent;
+      }
     }
 
-    if (action === "clear") {
-      console.log("clear key!");
-    }
-
-    if (action === "calculate") {
-      console.log("equal key!");
-    }
   }
 });
