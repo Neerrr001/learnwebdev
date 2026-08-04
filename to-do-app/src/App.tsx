1dadc5c43aa423bb708113ed087ae756326175e2
Neerrr001 <neeragarwal711@gmail.com>
2026-08-04 09:10:49 +0530

    created the basic vite setup again

---
+++ to-do-app/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h2>My todos</h2>
+      <input type="text" placeholder="enter" />
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default App
