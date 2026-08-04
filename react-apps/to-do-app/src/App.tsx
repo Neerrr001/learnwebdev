ab021d7f4542d0ea3243e4058e68161e7f3e6e52
Neerrr001 <neeragarwal711@gmail.com>
2026-08-04 08:48:45 +0530

    created a basic structure

---
+++ react-apps/to-do-app/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>My todos</h1>
+      <input type="text" placeholder="enter"/>
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default App
