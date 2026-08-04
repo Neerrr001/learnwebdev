43e0960fc71e124d4ac5483b908ccdd636e74e6b
Neerrr001 <neeragarwal711@gmail.com>
2026-08-04 10:17:52 +0530

    rendered my to do list

--- to-do-app/src/App.tsx
+++ to-do-app/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 function App() {
 
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState<string>("")
+  const [todos, setTodos] = useState<string[]>([])
 
   
   return (
@@ -12,8 +13,24 @@ function App() {
        placeholder="enter"
        value= {input}
        onChange={(e) => setInput(e.target.value)} />
-       <p>{input}</p>
-      <button>Submit</button>
+    
+      <button 
+        type="submit"
+        disabled = {input.trim() === ""}
+        onClick={()=>{
+          if(input.trim() === "")return; 
+          setTodos([...todos, input]);
+          setInput("")
+          console.log(todos)
+        }
+        }>
+          Submit
+        </button>
+        <ul>
+          {todos.map((todo,idx) =>(
+            <li key={idx}>{todo}</li>
+          ))}
+        </ul>
     </div>
   )
 }
