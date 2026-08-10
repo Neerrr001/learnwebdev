e35ce812b3481d7736463e3484d7d4bc13e5a8d0
Neerrr001 <neeragarwal711@gmail.com>
2026-08-10 19:18:57 +0530

    Created a InputText state to display the changes made my the user

--- to-do-app/src/components/EditCard.tsx
+++ to-do-app/src/components/EditCard.tsx
@@ -1,4 +1,5 @@
 import type {Todo} from "../types.ts"
+import { useState } from 'react'
 
 type EditCardProps = {
     todos: Todo[],
@@ -7,11 +8,35 @@ type EditCardProps = {
     setEditingTodo:React.Dispatch<React.SetStateAction<Todo | null>>
 }
 
+
 function EditCard({todos, setTodos, editingTodo, setEditingTodo}: EditCardProps) {
+    
+    const [InputText, setInputText] = useState(
+        editingTodo?.text ?? ""
+    )
+
+    function handleTodoChange(e: React.ChangeEvent<HTMLInputElement>){
+        setInputText(e.target.value)
+    }
+
+
   return (
     <div className="fixed flex justify-center items-center inset-0 bg-black/40">
-        <div className="w-96 p-6 rounded-xl shadow-xl bg-white ">
-            
+        <div className="w-86 min-h-76 p-6 rounded-xl shadow-xl bg-white ">
+            <div>
+                <h3>Edit Todo</h3>
+            </div>
+            <div>
+                {editingTodo &&
+                <input type="text"
+                value={InputText}
+                onChange={handleTodoChange} />}
+            </div>
+            <div>
+                <button>Cancel</button>
+                <button>Save</button>
+            </div>
+
         </div>
       
     </div>
