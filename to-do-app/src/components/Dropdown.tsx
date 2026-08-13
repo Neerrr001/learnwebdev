89713d4d084a9dfec40d49826916b7736935559e
Neerrr001 <neeragarwal711@gmail.com>
2026-08-13 12:46:19 +0530

    completed dropdown component

--- to-do-app/src/components/Dropdown.tsx
+++ to-do-app/src/components/Dropdown.tsx
@@ -3,20 +3,21 @@ import type { Todo } from "../types"
 
 type dropdownProps = {
     results: Todo[]
+    onResultClick: (todo:Todo)=>void
 }
 
-function Dropdown({results}:dropdownProps) {
+function Dropdown({results, onResultClick}:dropdownProps) {
   return (
     <div className="absolute ml-6 w-full bg-white rounded-lg top-full left-0 shadow-xl mt-1">
         {(results.length === 0)? (
-            <div className="p-3 hover:bg-gray-100 ">
+            <div className="p-3 text-gray-500 hover:bg-gray-100 ">
                 No matching results
             </div>
         ): (results.map((todo)=>(
-
             <div 
             key={todo.id}
-            className="p-3 cursor-pointer hover:bg-gray-100">
+            className="p-3 cursor-pointer hover:bg-gray-100"
+            onClick={()=> onResultClick(todo)}>
                {todo.title}
             </div>
 
